63464d9682e68ed1bd043cc8453951b914dc9665
Javier Esquiva Mira <javieresquivamira@gmail.com>
2021-10-26 23:32:32 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(500)
+})
